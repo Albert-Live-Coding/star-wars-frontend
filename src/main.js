2e323d12e627991ec070b26381769a7234c1e1bd
hannahnier <hannahr@posteo.de>
2024-03-22 11:13:22 +0100

    add people

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import { header } from './components/header.js';
-import { myFetch } from './utils/fetchData.js';
-export const body = document.querySelector('body');
+import { header } from "./components/header.js";
+// import { myFetch } from './utils/fetchData.js';
+export const body = document.querySelector("body");
+import {peopleArray, displayPeople} from "./components/people.js";
+
 
 body.appendChild(header());
 
-myFetch().then((data) => console.log(data));
+// myFetch().then((data) => console.log(data));
 
+displayPeople(peopleArray);
